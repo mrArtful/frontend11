62615a1927806a2b444b34ead602c17953582b4f
Roman Kutselepa <roman.kutselepa@gmail.com>
2024-05-04 13:03:08 +0300

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,49 @@
+// Задано полное имя в виде строки
+// Используйте переменную fullName
+// чтобы извлечь имя и фамилию в соответствующие переменные
+let fullName = 'Jack Smith';
+
+let firstName;
+let lastName;
+
+
+
+// используйте объект myCar чтобы вывести в консоль строку
+// "This is my Jaguar F-Type and it is black"
+let myCar = {
+    make: 'Jaguar',
+    model: 'F-Type',
+    color: 'black'
+}
+
+
+
+// Добавьте в объект profile -> info пару
+// height: 180  используя переменные
+let profile = {
+    userName: 'Terminator',
+    friends: 100000,
+    info: {
+        age: 50,
+        eyes: 'brown'
+    }
+}
+
+let keyToUse = 'height';
+let height = 180;
+
+
+// Добавьте в объект data две пары
+// name: 'Jupiter',
+// nameLength: 7
+// используйте переменную planet
+let planet = 'Jupiter';
+const data = {};
+
+
+
+// Используя переменную num
+// добавьте в объект numObj три пары, само число, квадрат числа и куб числа,
+// имена ключей произвольные
+let num = 5;
+let numObj = {};
